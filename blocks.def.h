92b2234cb8efd3846f4f343fa346726f4330d563
Derek Taylor <derek@distrotube.com>
2021-10-07 17:07:11 -0500

    Minor edits

--- blocks.def.h
+++ blocks.def.h
@@ -1,17 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
-    {" 🐧 ", "/opt/dwmblocks-distrotube-git/scripts/kernel",		    360,		        2},
+    {" 🐧 ", "/opt/dwmblocks-distrotube/scripts/kernel",		    360,		        2},
 
-	{" 🔺 ", "/opt/dwmblocks-distrotube-git/scripts/upt",		        60,		            2},
+	{" 🔺 ", "/opt/dwmblocks-distrotube/scripts/upt",		        60,		            2},
 
-	{" 📦 ", "/opt/dwmblocks-distrotube-git/scripts/pacupdate",		360,		        9},
+	{" 📦 ", "/opt/dwmblocks-distrotube/scripts/pacupdate",		360,		        9},
 	
-	{" 💻 ", "/opt/dwmblocks-distrotube-git/scripts/memory",	        6,		            1},
+	{" 💻 ", "/opt/dwmblocks-distrotube/scripts/memory",	        6,		            1},
 
-	{" 🔊 ", "/opt/dwmblocks-distrotube-git/scripts/volume",			2,		            10},
+	{" 🔊 ", "/opt/dwmblocks-distrotube/scripts/volume",			2,		            10},
 
-	{" 🕑 ", "/opt/dwmblocks-distrotube-git/scripts/clock",			5,		            0},
+	{" 🕑 ", "/opt/dwmblocks-distrotube/scripts/clock",			5,		            0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
